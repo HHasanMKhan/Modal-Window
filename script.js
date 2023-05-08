19f622cc0392c5220b5aa8d1cb14602fae72f3f2
Hasan Khan <hasanmullerkhan@gmail.com>
2023-05-08 16:16:52 +0100

    Show modal buttons now show modal and reveal overlay

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalBtn = document.querySelector('.close-modal');
+const showModalBtn = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', function () {
+    console.log(showModalBtn[i]);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
