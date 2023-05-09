3ba1e5d12315e985cf7ea17ff5fcd059152f239a
Hasan Khan <hasanmullerkhan@gmail.com>
2023-05-09 23:53:13 +0100

    Refactored open modal button functionality

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const showModalBtn = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < showModalBtn.length; i++) {
-  showModalBtn[i].addEventListener('click', function () {
-    console.log(showModalBtn[i]);
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', openModal);
+}
 
 closeModalBtn.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
